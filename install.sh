e9f555d7a116bd9e214890331a84f86b364efd3b
Fblade <lucas.toirkens@gmail.com>
2025-05-04 15:07:57 +0200

    Add zsh installation in script

--- install.sh
+++ install.sh
@@ -34,5 +34,23 @@ else
   echo "Error: .p10k.zsh not found!"
 fi
 
+# Check if zsh-autosuggestions is installed
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  echo "Installing zsh-autosuggestions plugin..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+else
+  echo "zsh-autosuggestions is already installed."
+fi
+
+# Make sure zsh-autosuggestions is enabled in .zshrc
+if ! grep -q "zsh-autosuggestions" "$HOME/.zshrc"; then
+  echo "Enabling zsh-autosuggestions plugin in .zshrc..."
+  sed -i '/^plugins=(/ s/)/ zsh-autosuggestions)/' "$HOME/.zshrc"
+else
+  echo "zsh-autosuggestions plugin is already enabled in .zshrc."
+fi
+
 echo "All dotfiles linked successfully."
 
