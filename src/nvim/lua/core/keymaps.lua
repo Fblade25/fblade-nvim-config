13bebd01365df453f0cf87af2ce95f3ca325fa40
Lucas Toirkens <lucas.toirkens@gmail.com>
2025-05-26 09:19:43 +0200

    Add live_grep to ctrl + f

--- src/nvim/lua/core/keymaps.lua
+++ src/nvim/lua/core/keymaps.lua
@@ -7,6 +7,9 @@ vim.g.mapleader = " "
 -- Open Telescope with Ctrl + P
 map("n", "<C-p>", "<cmd>Telescope find_files<CR>", opts)
 
+-- Open Telescope live_grep with Ctrl + [
+map("n", "<C-f>", ":Telescope live_grep<CR>", opts)
+
 -- File tree toggle
 map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", opts)
 
